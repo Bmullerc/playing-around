d1f4b8b187267611f8bd41a14a871c87e67289d1
Bernardo Muller <bernardo.muller.bm@gmail.com>
2022-12-26 14:25:01 -0300

    add: tailwind, change folder layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Inter } from '@next/font/google'
+import '../styles/globals.css'
+
+const inter = Inter({
+  variable: '--font-inter',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
